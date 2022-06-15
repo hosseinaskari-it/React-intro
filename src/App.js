0a727688f4149068d0922c3eb3aef4d0423f1197
Hossein Askari <hosseinaskari.it@gmail.com>
2022-06-16 00:31:26 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
+
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
